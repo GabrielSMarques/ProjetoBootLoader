8e37e8e25019592b06b95b8a9b3edc4cf6892390
phdosilva <phos@cin.ufpe.br>
2018-09-25 23:18:22 -0300

    Pode-se chamar de jogo

--- kernel.asm
+++ kernel.asm
@@ -8,15 +8,96 @@ start:
 	call initVideo
 	call initInfo
 
-	.gameloop:
-		; call board
-		call ball
-		call bar
-		call moveBall
-		call gameControl
-		jmp .gameloop
+	 .gameloop:
+	 	call conferir
+	 	; call board
+	 	call ball
+		call drawBlocos
+	 	call bar
+	 	call moveBall
+	 	call gameControl
+	 	cmp word[game_flag], 1
+	 	je .gameloop
 ; 
 	; jmp done
+
+conferir:
+	mov bx, word[quantidade]
+	mov word[atual], blocos
+	mov si, word[atual]
+
+	mov word[game_flag], 0
+	.loop1:
+		add si, 8
+		lodsw
+		cmp ax, 0
+		jne .on
+		dec bx
+		cmp bx, 0
+		jg .loop1
+	jmp .of
+	.on:
+	mov word[game_flag], 1
+	.of:
+	ret
+
+drawBlocos:
+	mov bx, word[quantidade]
+	mov word[atual], blocos
+	mov si, word[atual]
+	sub si, 10
+	mov word[atual], si
+
+	.loopzao:
+		mov si, word[atual]
+		add si, 10
+		mov word[atual], si
+
+		add si, 8
+		lodsw
+		cmp ax, 0
+		mov si, word[atual]
+		je .fimloopzao
+
+
+
+		lodsw
+		mov cx, ax
+		add si, 4
+		lodsw
+		add cx, ax
+
+		.innerloop:
+			mov si, word[atual]
+			add si, 2
+			lodsw
+			mov dx, ax
+			add si, 2
+			lodsw
+			add dx, ax
+			.otherloop:
+				push bx
+				mov si, word[atual]
+				add si, 4
+				lodsw
+				call writePixel
+				pop bx
+
+				dec dx
+				mov si, word[atual]
+				add si, 2
+				cmp dx, word[si]
+				jg .otherloop
+			dec cx
+			mov si, word[atual]
+			cmp cx, word[si]
+			jg .innerloop
+		.fimloopzao:
+		dec bx
+		cmp bx, 0
+		jg .loopzao
+
+	ret
 
 moveBall:
 	pusha
@@ -41,6 +122,143 @@ moveBall:
 	add cx, word[velocidade]								;
 	.esquerda:												;
 
+	push bx
+	mov bx, word[quantidade]
+	mov word[atual], blocos
+	mov si, word[atual]
+	sub si, 10
+	mov word[atual], si
+
+	.loopzao:
+		mov si, word[atual]
+		add si, 10
+		mov word[atual], si
+
+		add si, 8
+		lodsw
+		cmp ax, 0
+		mov si, word[atual]
+		je .fimloopzao
+
+
+
+		mov si, word[atual]
+
+		lodsw
+		cmp cx, ax
+		jl .nada1
+		add si, 4
+		add ax, word[si]
+		cmp cx, ax
+		jg .nada1
+
+		mov si, word[atual]
+		add si, 2
+		lodsw
+		cmp dx, ax
+		jl .nada1
+		add si, 2
+		add ax, word[si]
+		cmp dx, ax
+		jg .nada1
+
+		mov si, word[atual]	
+		add si, 8
+		mov word[si], 0
+		push cx
+		push dx
+			mov si, word[atual]
+			push ax
+			mov ax, word[si]
+			mov word[aux_x], ax
+			add si, 2
+			mov ax, word[si]
+			mov word[aux_y], ax
+			pop ax
+			mov cx, word[aux_x]
+			add cx, word[tam]
+			.laco1:
+				mov dx, word[aux_y]
+				add dx, word[tam]
+				.laco2:
+					mov al, 0
+					call writePixel
+					dec dx
+					cmp dx, word[aux_y]
+					jge .laco2
+				dec cx
+				cmp cx, word[aux_x]
+				jge .laco1
+			
+		pop dx
+		pop cx
+
+		.fimloopzao:
+		dec bx
+		cmp bx, 0
+		jg .loopzao
+
+
+	mov word[atual], blocos
+	mov si, word[atual]
+	add si, 8
+	lodsw
+	cmp ax, 0
+	je .nada1
+
+	; mov si, word[atual]
+
+	; lodsw
+	; cmp cx, ax
+	; jl .nada1
+	; add si, 4
+	; add ax, word[si]
+	; cmp cx, ax
+	; jg .nada1
+
+	; mov si, word[atual]
+	; add si, 2
+	; lodsw
+	; cmp dx, ax
+	; jl .nada1
+	; add si, 2
+	; add ax, word[si]
+	; cmp dx, ax
+	; jg .nada1
+
+	; mov si, word[atual]	
+	; add si, 8
+	; mov word[si], 0
+	; push cx
+	; push dx
+	; 	mov si, word[atual]
+	; 	push ax
+	; 	mov ax, word[si]
+	; 	mov word[aux_x], ax
+	; 	add si, 2
+	; 	mov ax, word[si]
+	; 	mov word[aux_y], ax
+	; 	pop ax
+	; 	mov cx, word[aux_x]
+	; 	add cx, word[tam]
+	; 	.laco1:
+	; 		mov dx, word[aux_y]
+	; 		add dx, word[tam]
+	; 		.laco2:
+	; 			mov al, 0
+	; 			call writePixel
+	; 			dec dx
+	; 			cmp dx, word[aux_y]
+	; 			jge .laco2
+	; 		dec cx
+	; 		cmp cx, word[aux_x]
+	; 		jge .laco1
+		
+	; pop dx
+	; pop cx
+
+	.nada1:
+	pop bx
 	cmp cx, word[limite_esquerda]							;verificando limites da tela
 	jl .skip1												;
 	cmp cx, word[limite_direita]							;
@@ -60,9 +278,33 @@ moveBall:
 	cmp dx, word[limite_cima]								
 	jl .skip3
 	cmp dx, word[limite_baixo]
+	jg .skip_fim
+
+	mov ax, word[player_PosX]
+	sub ax, word[ball_Raio]
+	cmp cx, ax
+	jl .pula1
+
+	mov ax, word[player_PosX]
+	push bx
+	xor bx, bx
+	mov bl, byte[player_TamX]
+	add ax, bx
+	pop bx
+	add ax, word[ball_Raio]
+	cmp cx, ax
+	jg .pula1
+
+	mov ax, word[player_PosY]
+	sub ax, word[ball_Raio]
+	cmp dx, ax
 	jg .skip3
+
+	.pula1:
 	mov word[ball_PosY], dx
 	jmp .skip4
+	.skip_fim:
+	mov word[game_flag], 0
 	.skip3:
 	cmp bh, 0
 	je .zero_vertical
@@ -286,37 +528,63 @@ initInfo:
 
 board:
 	mov al, 4
-	mov dx, 150
+	mov dx, 0
 	.innerloop:
-		mov cx, 100
+		mov cx, 0
 		.otherloop:
 			call writePixel
 			inc cx
-			cmp cx, 220
+			cmp cx, 50
 			jl .otherloop
 		inc dx
-		cmp dx, 200
+		cmp dx, 50
 		jl .innerloop
 	ret
 
-
 done:
     jmp $
+
+quantidade dw 2
+
+atual dw 0
+
+tam dw 10
+
+blocos:
+	dw 20 ;x
+	dw 10 ;y
+	dw 10 ;cor
+	dw 10 ;tam
+	dw 1 ;flag
+
+	dw 30
+	dw 10
+	dw 1
+	dw 10
+	dw 1
+
+	dw 50
+	dw 10
+	dw 10
+	dw 10
+	dw 1
+
+game_flag dw 1
 
 limite_esquerda dw 0
 limite_direita dw 319
 limite_cima dw 0
 limite_baixo dw 199
 
-sentido_vertical db 0
+sentido_vertical db 1
 sentido_horizontal db 0
 
 ball_Dia db 0
 ball_Raio dw 5
 ball_RaioQ dw 0
-ball_PosX dw 144
-ball_PosY dw 182
-velocidade dw 1
+ball_PosX dw 159
+ball_PosY dw 172
+velocidade dw 2
 
 player_PosX dw 144
 player_PosY dw 182
@@ -325,6 +593,9 @@ player_TamX db 30
 player_TamY db 8	
 player_Color db 1
 player_Borda db 5
+
+aux_x dw 0
+aux_y dw 0
 
 times (512 * 2)-($-$$) db 0		
 dw 0xaa55			
